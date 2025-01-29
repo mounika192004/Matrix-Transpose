b595976fb1ba3af279f97aaa6b1c995acee27c25
Mounika <nagargojemounika@gmail.com>
2025-01-29 05:30:25 +0530

    Add files via upload

---
+++ Transpose2DArray.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Transpose2DArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        System.out.print("Enter the number of columns: ");
+        int cols = scanner.nextInt();
+
+        int[][] array = new int[rows][cols];
+
+        System.out.println("Enter elements of the array:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                array[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Original array:");
+        printArray(array);
+
+        int[][] transposedArray = transposeArray(array);
+
+        System.out.println("Transposed array:");
+        printArray(transposedArray);
+
+        scanner.close();
+    }
+
+    public static int[][] transposeArray(int[][] array) {
+        int rows = array.length;
+        int cols = array[0].length;
+        int[][] transposedArray = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposedArray[j][i] = array[i][j];
+            }
+        }
+
+        return transposedArray;
+    }
+
+    public static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
